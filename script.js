14d3e17dc28b088779b3faef04e7355ec83adf9f
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-03-24 17:05:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function togglePassword() {
+  let password = document.getElementById("password");
+
+  if (password.type === "password") {
+    password.type = "text";
+  } else {
+    password.type = "password";
+  }
+}
