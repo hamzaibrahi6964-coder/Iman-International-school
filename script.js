c9345bed4bedb55037d0b0b75af3e2f3fce35d6f
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-03-24 17:22:37 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function togglePassword() {
-  let password = document.getElementById("password");
-
-  if (password.type === "password") {
-    password.type = "text";
-  } else {
-    password.type = "password";
-  }
-}
